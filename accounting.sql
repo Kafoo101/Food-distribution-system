a339e9f415d6f360b7cfd783e97634e6d30943ad
Kafoo101 <darrenang7722@gmail.com>
2025-12-25 23:55:06 +0800

    Create accounting.sql

---
+++ accounting.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 25, 2025 at 04:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accounting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_id` varchar(10) NOT NULL,
+  `company_name` varchar(50) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `address` varchar(80) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `operating` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`company_id`, `company_name`, `city`, `address`, `phone`, `operating`) VALUES
+('C-0001', 'Keelung Banana Starlight Corp. Inc.', 'Keelung', 'Zhongzheng Rd. No. 7', '+886 918 877 688', 1),
+('C-0002', 'Taipei Soulglad TM', 'Taipei', 'Zhongxiao E. Rd. No. 7', '+886 962 344 578', 1),
+('C-0003', 'Doraemon', 'Tainan', 'bla bla bla', '111111111', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `item_id` varchar(10) NOT NULL,
+  `item_name` varchar(20) NOT NULL,
+  `stock` int(7) NOT NULL DEFAULT 1,
+  `onsale` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_id`, `item_name`, `stock`, `onsale`) VALUES
+('I-0001', 'Coca-Cola', 60, 1),
+('I-0002', 'Banana', 100, 1),
+('I-0003', 'Chicken', 100, 1),
+('I-0004', 'Garlic', 147, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `purchase_id` varchar(10) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
+  `item_id` varchar(10) NOT NULL,
+  `quantity` int(7) NOT NULL DEFAULT 1,
+  `purchase_price` int(10) NOT NULL,
+  `company_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `purchase`
+--
+
+INSERT INTO `purchase` (`purchase_id`, `timestamp`, `item_id`, `quantity`, `purchase_price`, `company_id`) VALUES
+('P-0001', '2021-10-29 15:07:24', 'I-0001', 55, 50, 'C-0002'),
+('P-0002', '2025-12-03 17:06:25', 'I-0003', 20, 150, 'C-0003'),
+('P-0003', '2025-12-16 17:06:25', 'I-0004', 20, 40, 'C-0003'),
+('P-0004', '2025-12-25 16:51:17', 'I-0004', 12, 30, 'C-0002');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sales_id` varchar(10) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
+  `item_id` varchar(10) NOT NULL,
+  `quantity` int(7) NOT NULL DEFAULT 1,
+  `sales_price` int(10) NOT NULL,
+  `company_id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`sales_id`, `timestamp`, `item_id`, `quantity`, `sales_price`, `company_id`) VALUES
+('S-0001', '2025-12-25 17:24:08', 'I-0001', 20, 50, 'C-0001');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`),
+  ADD UNIQUE KEY `company_name` (`company_name`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`purchase_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `company_id` (`company_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `company_id` (`company_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
